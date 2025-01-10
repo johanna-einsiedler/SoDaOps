068f66437992de7f5a57b3e25cc00ffd62a06f2b
Marten Appel <martenappel94@gmail.com>
2025-01-10 16:02:32 +0100

    first draft visualization and evaluation of model

--- src/tweet_sentiment_anlaysis/model.py
+++ src/tweet_sentiment_anlaysis/model.py
@@ -6,10 +6,15 @@ logger.remove()
 logger.add(sys.stdout, level="DEBUG")
 
 class SentimentModel:
-    def __init__(self, model_path="cardiffnlp/twitter-roberta-base-sentiment-latest"):
+    #def __init__(self, model_path="cardiffnlp/twitter-roberta-base-sentiment-latest"):
+    def __init__(self, model_path="distilbert-base-uncased-finetuned-sst-2-english"):
+        
         self.model_path = model_path
         self.pipe = pipeline("sentiment-analysis", model=self.model_path, tokenizer=self.model_path)
         logger.info(f"Model initialized with path: {self.model_path}")
+
+    def predict(self, text):
+        return self.pipe(text)
 
 if __name__ == "__main__":
     model = SentimentModel()
