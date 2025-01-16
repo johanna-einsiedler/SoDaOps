4191819d6f87d8197bef071ab0e69d6d72a75a58
johanna-einsiedler <j.eremita@gmx.at>
2025-01-16 15:32:41 +0100

    fixed folder structure

---
+++ tests/__init__.py
@@ -0,0 +1,4 @@
+import os
+_TEST_ROOT = os.path.dirname(__file__)  # root of test folder
+_PROJECT_ROOT = os.path.dirname('../tweet_sentiment_analysis/')   # root of project
+_PATH_DATA = "data" # root of data
--- tests/test_data.py
+++ tests/test_data.py
@@ -1,9 +1,13 @@
 from torch.utils.data import Dataset
-
-from tweet_sentiment_anlaysis.data import MyDataset
+from datasets import load_dataset
+from tweet_sentiment_analysis.data import *
+import os
+from tests import _PATH_DATA
 
 
 def test_my_dataset():
-    """Test the MyDataset class."""
-    dataset = MyDataset("data/raw")
-    assert isinstance(dataset, Dataset)
+    csv_dir = _PATH_DATA + '/processed'
+    data_files = {"train": os.path.join(csv_dir, "train.csv"),  "test": os.path.join(csv_dir, "test.csv"), "val": os.path.join(csv_dir, "val.csv")}
+    dataset = load_dataset("csv", data_files=data_files)
+    assert dataset.shape['train'][0] == dataset.shape['val'][0] == dataset.shape['test'][0], "Train,test and validation data don't have same shape"
+
