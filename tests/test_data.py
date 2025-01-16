3fc2c12a03daaa83374eac0ba61d9e8e8fbb3afe
johanna-einsiedler <j.eremita@gmx.at>
2025-01-16 16:33:11 +0100

    added caching

--- tests/test_data.py
+++ tests/test_data.py
@@ -4,10 +4,10 @@ from tweet_sentiment_analysis.data import *
 import os
 from tests import _PATH_DATA
 
-
+data_dir = _PATH_DATA + '/processed'
+@pytest.mark.skipif(not os.path.exists(data_dir), reason="Data files not found")
 def test_my_dataset():
-    csv_dir = _PATH_DATA + '/processed'
-    data_files = {"train": os.path.join(csv_dir, "train.csv"),  "test": os.path.join(csv_dir, "test.csv"), "val": os.path.join(csv_dir, "val.csv")}
+    data_files = {"train": os.path.join(data_dir, "train.csv"),  "test": os.path.join(data_dir, "test.csv"), "val": os.path.join(data_dir, "val.csv")}
     dataset = load_dataset("csv", data_files=data_files)
     assert dataset.shape['train'][0] == dataset.shape['val'][0] == dataset.shape['test'][0], "Train,test and validation data don't have same shape"
 
