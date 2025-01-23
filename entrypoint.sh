3227c4f98b4076f4d0e51a61a72ef30a131a6fd8
Magnus Nielsen <magnus.nielsen@sodas.ku.dk>
2025-01-23 11:45:56 +0100

    Update to support wandb run count env var

--- entrypoint.sh
+++ entrypoint.sh
@@ -29,8 +29,8 @@ echo "Sweep created with ID: $sweep_id"
 export WANDB_SWEEP_NAME=$(echo "$sweep_id" | awk -F'/' '{print $NF}')
 echo "Sweep name: $WANDB_SWEEP_NAME"
 # Start the W&B agent
-echo "Starting W&B agent for sweep: $sweep_id"
-wandb agent "$sweep_id"
+echo "Starting W&B agent for sweep: $WANDB_SWEEP_NAME"
+wandb agent --count "$((WANDB_RUN_COUNT))" "$WANDB_ENTITY/$WANDB_PROJECT/$WANDB_SWEEP_NAME"
 # Test inference with single tweet
 echo "Testing inference with single tweet"
 python src/tweet_sentiment_analysis/model.py
