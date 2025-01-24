2dec4d416ce7e8431ceba69bc01e475f906954e8
Magnus Nielsen <magnus.nielsen@sodas.ku.dk>
2025-01-24 16:22:45 +0100

    Update tests for new model class

--- tests/test_model.py
+++ tests/test_model.py
@@ -4,14 +4,6 @@ from unittest.mock import MagicMock, patch
 from tweet_sentiment_analysis.model import SentimentPipeline
 
 
-# Test for `SentimentPipeline` class
-@patch("tweet_sentiment_analysis.model.pipeline")
-@patch("tweet_sentiment_analysis.model.load_sentiment_pipeline")
-def test_sentiment_pipeline_default(mock_load_sentiment_pipeline, mock_pipeline):
-    mock_pipeline.return_value = "default_pipeline_instance"
-    mock_load_sentiment_pipeline.side_effect = Exception("Mocked exception")
-
-
 # Test `SentimentPipeline.predict`
 @patch("tweet_sentiment_analysis.model.SentimentPipeline")
 def test_sentiment_pipeline_predict(mock_pipe):
